f924ac5a4794295195913415695932285c54ffe2
Zunon <zunon@zunon.io>
2018-06-11 18:10:31 +0400

    Add and remove uppercase roles

--- src/handlers.ts
+++ src/handlers.ts
@@ -6,7 +6,7 @@ export function addRole(message: Message, commandArguments: string[]) {
 
   commandArguments.forEach(roleName => {
     guild.fetchMember(message.author.id).then(user => {
-      user.addRole(guild.roles.find(`name`, roleName)).then(
+      user.addRole(guild.roles.find(`name`, roleName.toUpperCase())).then(
         user => {
           user.send(`Successfully added role!`)
         },
@@ -24,7 +24,7 @@ export function removeRole(message: Message, commandArguments: string[]) {
 
   commandArguments.forEach(roleName => {
     guild.fetchMember(message.author.id).then(user => {
-      user.removeRole(guild.roles.find(`name`, roleName)).then(
+      user.removeRole(guild.roles.find(`name`, roleName.toUpperCase())).then(
         user => {
           user.send(`Successfully removed role!`)
         },
